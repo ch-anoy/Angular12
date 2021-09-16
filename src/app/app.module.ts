d11b39bbccc471d7e7fc820be2c7337b001287c1
anoy chowdhury <anoy@eatlbd.com>
2021-09-16 12:47:38 +0600

    components add,module,css etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponent } from './second/second.component';
+import {ThirdModule} from './third/third.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ThirdModule
   ],
   providers: [],
   bootstrap: [AppComponent]
